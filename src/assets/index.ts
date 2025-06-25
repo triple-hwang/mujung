50b287d572b08ed2d3bbc92bc6b55656f7497635
justn-hyeok <justn.hyeok@gmail.com>
2025-06-25 12:31:14 +0900

    chore(#1): 프로젝트 기본 폴더 구조 생성
    - src/ 하위 폴더 구조 생성
    - 각 폴더에 index.ts 배럴 파일 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,3 @@
+// 에셋 export 예시
+// export { ReactComponent as Logo } from './logo.svg';
+// export { ReactComponent as Icon } from './icon.svg';
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+// 페이지 컴포넌트 export 예시
+// export { default as MainPage } from './MainPage';
+// export { default as VotePage } from './VotePage';
