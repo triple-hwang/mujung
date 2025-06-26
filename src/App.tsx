a16e315eee8665d31e5065e461b2afd48eea67fc
justn-hyeok <justn.hyeok@gmail.com>
2025-06-26 19:48:23 +0900

    chore(#2): Lint Error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import { MainPage, VotePage, DashboardPage, NotFoundPage } from './pages';
+import { DashboardPage, MainPage, NotFoundPage, VotePage } from './pages';
 
 function App() {
   return (
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as MainPage } from './MainPage';
-export { default as VotePage } from './VotePage';
 export { default as DashboardPage } from './DashboardPage';
+export { default as MainPage } from './MainPage';
 export { default as NotFoundPage } from './NotFoundPage';
+export { default as VotePage } from './VotePage';
