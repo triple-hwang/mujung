0bb51cda9c11cf557063a0bd1bef2bb91d31c4f6
justn-hyeok <justn.hyeok@gmail.com>
2025-07-06 04:26:48 +0900

    feat(#13): assets 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,7 @@
 export { default as Logo } from './logo.svg';
+export { default as SpotifyIcon } from './spotify.svg';
+export { default as YoutubeIcon } from './youtube.svg';
+export { default as SubmitImg } from './submitImg.svg';
+export { default as VoteImg } from './voteImg.svg';
+
+export { default as FreesentationFont } from './FreesentationVF.ttf';
