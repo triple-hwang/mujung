c8f0aa812c821e99b281035cf0a41eb809309084
hjy080530 <bomaye119@gmail.com>
2025-07-08 14:00:23 +0900

    chore :: 쿠키 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+axios.defaults.withCredentials = true;
 const api = axios.create({
     baseURL: 'https://mujung-back-fastapi.onrender.com',
+    withCredentials: true,
 });
 
 export const fetchSongs = async () => {
